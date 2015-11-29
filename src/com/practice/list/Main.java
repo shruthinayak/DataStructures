5da33b50aa347ee0f9a60b0ba3ad021e430b810e
shruthinayak <rn.shruthi@gmail.com>
2015-11-29 15:41:00 -0600

    DataStructures - List

---
+++ src/com/practice/list/Main.java
@@ -0,0 +1,147 @@
+package com.practice.list;
+
+import java.util.HashMap;
+
+//forward case addition of linked list
+//partition on x
+
+public class Main {
+	static int[] data = { 2, 3, 6, 8, 11, 8, 6, 3, 5, 1 };
+	static Node head = linkedListDemo(data);
+
+	public static void main(String[] args) {
+		// addLinkedLists();
+		// initiateDelete(3);
+		// detectLoopStartPoint(head);
+		reverseLinkedList(head);
+		// listPalindrome(linkedListDemo(a));
+		
+		
+
+	}
+	
+	
+	
+	public static void kthElementFromEnd(Node head, int k) {
+		Node node = head;
+		while (node != null && k != 0) {
+			node = node.next;
+			k--;
+		}
+		Node n = head;
+		while (node != null) {
+			node = node.next;
+			n = n.next;
+		}
+		System.out.println(n.data);
+	}
+
+	private static void initiateDelete(int n) {
+		Node nodeTBD = head;
+		while (n != 0) {
+			n--;
+			nodeTBD = nodeTBD.next;
+		}
+		nodeTBD.printList(head);
+		deleteWithOnlyReference(nodeTBD);
+	}
+
+	private static Node result;
+
+	static Node linkedListDemo(int[] a) {
+		Node head = new Node(1);
+		for (int d : a) {
+			head.appendToTail(d);
+		}
+		head.printList(head);
+		return head;
+	}
+
+	private static int add(Node h, Node j) {
+		if (h.next == null) {
+			return h.data + j.data;
+		} else {
+			Node n = new Node(add(h.next, j.next));
+			int sum = (h.data + j.data);
+			Node x = new Node(sum);
+			x.next = n;
+			result = x;
+			return 0;
+		}
+	}
+
+	// Implement an algorithm to delete a node c only when access is given to it.
+	private static void deleteWithOnlyReference(Node nodeTBD) {
+		System.out.println("After deleting " + nodeTBD.data);
+		Node nextPtr = nodeTBD.next;
+		nodeTBD.data = nextPtr.data;
+		nodeTBD.next = nextPtr.next;
+		head.printList(head);
+	}
+
+	// detect beginning of a loop in a linked list
+	private static void detectLoopStartPoint(Node head) {
+		Node p = head;
+		Node q = head.next.next;
+		while (p != q) {
+			p = p.next;
+			q = q.next.next;
+		}
+		System.out.println("Detected a loop: Merge point at " + p.data);
+		p = head;
+		while (p != q) {
+			p = p.next;
+			q = q.next.next;
+		}
+		System.out.println("Beginning of the loop is " + p.data);
+	}
+
+	private static Node reverseLinkedList(Node head1) {
+		Node temp = head1; //one
+		head1 = head1.next; //two
+		temp.next = null;
+		System.out.println(head1.data);
+		Node an = head1.next; //three
+		while (head1.next != null) { 
+			head1.next = temp; //two.next = one;
+			temp = head1;//one = two
+			head1 = an; //two = three
+			an = head1.next; //three = three.next
+			temp.printList(temp);
+		}
+		head1.next = temp;
+		temp = head1;
+		temp.printList(temp);
+		return temp;
+	}
+
+	// To check if a list is palindrome
+	private static void listPalindrome(Node head) {
+		int size = 0;
+		Node temp = head;
+		while (temp != null) {
+			size++;
+			temp = temp.next;
+		}
+		int mid = size / 2;
+		temp = head;
+		int i = 0;
+		while (i != mid) {
+			temp = temp.next;
+			i++;
+		}
+		Node temp2 = reverseLinkedList(temp);
+		temp2.printList(temp2);
+		temp = head;
+		head.printList(head);
+		while (temp2 != null) {
+			if (temp2.data != temp.data) {
+				System.out.println("Not a palindrome");
+				return;
+			}
+			temp = temp.next;
+			temp2 = temp2.next;
+		}
+		System.out.println("Yayy! A palindrome.");
+	}
+}
---
+++ src/com/practice/list/Node.java
@@ -0,0 +1,232 @@
+package com.practice.list;
+
+public class Node {
+	Node next = null;
+	int data;
+
+	public Node(int d) {
+		data = d;
+	}
+
+	Node appendToTail(int d) {
+		Node end = new Node(d);
+		Node n = this;
+		while (n.next != null) {
+			n = n.next;
+		}
+		n.next = end;
+		return end;
+	}
+
+	void deleteFromList(Node head, int d) {
+		Node pointer = head;
+		while (pointer.next != null && pointer.next.data != (d)) {
+			pointer = pointer.next;
+		}
+		pointer.next = pointer.next.next;
+	}
+
+	void printList(Node head) {
+		Node pointer = head;
+		while (pointer != null) {
+			System.out.print(pointer.data + "->");
+			pointer = pointer.next;
+		}
+		System.out.println();
+	}
+
+	Node InsertNth(Node head, int data, int position) {
+
+		Node newNode = new Node(data);
+
+		if (head == null) {
+			return newNode;
+		}
+		Node temp = head;
+		Node prev = null;
+		for (int find = 0; find < position; find++) {
+			prev = temp;
+			temp = temp.next;
+			if (temp == null) {
+				break;
+			}
+		}
+		if (position == 0) {
+			newNode.next = head;
+			head = newNode;
+			return head;
+		} else if (temp == null) {
+			prev.next = newNode;
+			return head;
+		} else {
+			newNode.next = temp;
+			prev.next = newNode;
+			return head;
+		}
+
+	}
+
+	Node Delete(Node head, int position) {
+		// Complete this method
+		if (head == null) {
+			return null;
+		}
+		if (position == 0) {
+			return head.next;
+		}
+		Node temp = head;
+		Node prev = null;
+		for (int i = 0; i < position; i++) {
+			prev = temp;
+			temp = temp.next;
+			if (temp == null) {
+				return null;
+			}
+		}
+		prev.next = temp.next;
+		return head;
+	}
+
+	int HasCycle(Node head) {
+		Node one = head;
+		Node two = head;
+		while (one != null && two != null && two.next != null) {
+			one = one.next;
+			two = two.next.next;
+			if (one == two) {
+				return 1;
+			}
+		}
+		return 0;
+	}
+
+	int FindMergeNode(Node headA, Node headB) {
+		// Complete this function
+		// Do not write the main method.
+		int countA = 0;
+		int countB = 0;
+		Node tempA = headA;
+		Node tempB = headB;
+		while (tempA != null) {
+			tempA = tempA.next;
+			countA++;
+		}
+		while (tempB != null) {
+			tempB = tempB.next;
+			countB++;
+		}
+		int k = Math.abs(countA - countB);
+		tempA = headA;
+		tempB = headB;
+		if (countA >= countB) {
+			while (k != 0) {
+				tempA = tempA.next;
+				k--;
+			}
+		} else {
+			while (k != 0) {
+				tempB = tempB.next;
+				k--;
+			}
+		}
+
+		while (tempA != null) {
+			if (tempA == tempB) {
+				return tempA.data;
+			}
+			tempA = tempA.next;
+			tempB = tempB.next;
+		}
+		return -1;
+
+	}
+
+	void ReversePrint(Node head) {
+
+		if (head == null) {
+			return;
+		}
+		ReversePrint(head.next);
+		System.out.println(head.data);
+		return;
+
+	}
+
+	int CompareLists(Node headA, Node headB) {
+
+		while (headA != null && headB != null) {
+			if (headA.data != headB.data) {
+				return 0;
+			}
+			headA = headA.next;
+			headB = headB.next;
+		}
+		boolean f = headA == null && headB != null;
+		boolean g = headB == null && headA != null;
+
+		if (f || g)
+			return 0;
+		return 1;
+
+	}
+
+	Node MergeLists(Node l1, Node l2) {
+		Node p1 = l1;
+		Node p2 = l2;
+
+		Node fakeHead = new Node(0);
+		Node p = fakeHead;
+
+		while (p1 != null && p2 != null) {
+			if (p1.data <= p2.data) {
+				p.next = p1;
+				p1 = p1.next;
+			} else {
+				p.next = p2;
+				p2 = p2.next;
+			}
+
+			p = p.next;
+		}
+
+		if (p1 != null)
+			p.next = p1;
+		if (p2 != null)
+			p.next = p2;
+
+		return fakeHead.next;
+
+	}
+
+	int getNodeFromTail(Node head, int n) {
+
+		Node one = head;
+		Node two = head;
+		while (two != null && n != 0) {
+			two = two.next;
+			n--;
+		}
+		while (two.next != null) {
+			one = one.next;
+			two = two.next;
+		}
+		return one.data;
+
+	}
+	
+	Node RemoveDuplicates(Node head) {
+		  Node prev = head;
+		  Node nex = head;
+		    while(nex!=null){
+		        if(nex.data == prev.data){
+		            nex = nex.next;
+		            prev.next = null;
+		        } else{
+		            prev.next = nex;
+		            prev = prev.next;
+		        }
+		    }
+		    return head;
+		}
+
+}
