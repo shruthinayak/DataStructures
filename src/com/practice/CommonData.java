000431b715f4032983377e2089958cf87df20abd
shruthinayak <rn.shruthi@gmail.com>
2015-11-30 15:18:14 -0600

    Sort - MergeSort. Modifications

--- src/com/practice/CommonData.java
+++ src/com/practice/CommonData.java
@@ -5,4 +5,5 @@ package com.practice;
  */
 public class CommonData {
     public static int[] array = { 1, 2, 3, 4, 6, 7, 8, 9, 10 };
+    public static int[] unsorted = {  2, 3, 1, 4, 13, 7, 14, 9, 10 };
 }
---
+++ src/com/practice/sort/MergeSort.java
@@ -0,0 +1,98 @@
+package com.practice.sort;
+
+import com.practice.CommonData;
+
+import java.util.Arrays;
+
+/**
+ * Created by shruthi on 29/11/15.
+ */
+public class MergeSort {
+    static int[] array = CommonData.unsorted;
+    static int[] tempMergArr = new int[array.length];
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(array));
+        doMergeSort(0, array.length-1);
+        System.out.println(Arrays.toString(array));
+    }
+    private static void doMergeSort(int lowerIndex, int higherIndex) {
+
+        if (lowerIndex < higherIndex) {
+            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+            // Below step sorts the left side of the array
+            doMergeSort(lowerIndex, middle);
+            // Below step sorts the right side of the array
+            doMergeSort(middle + 1, higherIndex);
+            // Now merge both sides
+            mergeParts(lowerIndex, middle, higherIndex);
+        }
+    }
+
+    private static void mergeParts(int lowerIndex, int middle, int higherIndex) {
+
+        for (int i = lowerIndex; i <= higherIndex; i++) {
+            tempMergArr[i] = array[i];
+        }
+        int i = lowerIndex;
+        int j = middle + 1;
+        int k = lowerIndex;
+        while (i <= middle && j <= higherIndex) {
+            if (tempMergArr[i] <= tempMergArr[j]) {
+                array[k] = tempMergArr[i];
+                i++;
+            } else {
+                array[k] = tempMergArr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= middle) {
+            array[k] = tempMergArr[i];
+            k++;
+            i++;
+        }
+
+    }
+    /*public static void mergeSort(int[] a, int s, int e) {
+
+        if (s <= e) {
+            int mid = (s + e) / 2;
+            mergeSort(a, s, mid - 1);
+            mergeSort(a, mid + 1, e);
+            merge(a, s, mid, e);
+        }
+    }
+
+    public static int[] merge(int[] a, int s, int mid, int e) {
+
+        int m = mid - s + 1;
+        int n = e - mid + 1;
+        int[] c = new int[m];
+        int[] d = new int[n];
+        int[] b = new int[m + n];
+        int i = s, j=mid+1, k=0;
+        while (i < mid && j < e) {
+            if (a[i] <= a[j]) {
+                b[k] = a[i];
+                i++;
+            } else {
+                b[k] = a[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < mid) {
+            b[k] = a[i];
+            i++;
+            k++;
+        }
+        while (j < e) {
+            b[k] = a[j];
+            k++;
+            j++;
+        }
+        System.out.println(Arrays.toString(b));
+        return b;
+    }*/
+}
